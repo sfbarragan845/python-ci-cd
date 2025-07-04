b76a7b593c4341bc84049f08818cab3af771ad84
sfbarragan845 <stevenfernando-22@hotmail.com>
2025-07-03 23:13:46 -0500

    update test funcionality

--- tests/test_app.py
+++ tests/test_app.py
@@ -23,7 +23,7 @@ def test_suma(client):
 
 def test_saludo(client):
     r = client.get('/saludo/Rox')
-    assert '¡Hola Rox!' in r.get_data(as_text=True)
+    assert '"saludo": "¡Hola Rox!"' in r.get_data(as_text=True)
 
 def test_multiplicar(): assert multiplicar(2, 3) == 6
 def test_es_par(): assert es_par(4)
